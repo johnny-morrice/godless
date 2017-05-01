e9636900593e4c14cf6bc878c0941672d35f453a
John Morrice <john@functorama.com>
2017-05-01 11:27:33 +0100

    Skeleton for Namespace unit tests

--- TODO.md
+++ TODO.md
@@ -2,10 +2,15 @@
 
 1. Unit tests
 2. Namespace equality (get rid of hacky test eq functions)
-3. Replication
-4. Functional & integration tests
+3. Rethink indexing
+4. Replication
+5. Functional & integration tests
 
 I want to do the replication bit before writing integration tests via docker because I think the API might change.  As a one man project, doing it in this order is less costly.
+
+Indexing should be rethought to make replication easy as possible.  Currently we store data and indexes in the same pages on IPFS.  This is harder to replicate than simply an index table.  
+
+Imagine another peer downloads the index and wants to check it has access to all the same data.
 
 # Test priority order
 
---
+++ namespace_test.go
@@ -0,0 +1,121 @@
+package godless
+
+import (
+	"testing"
+)
+
+func TestEmptyNamespace(t *testing.T) {
+	t.Fail()
+}
+
+func TestMakeNamespace(t *testing.T) {
+	t.Fail()
+}
+
+func TestNamespaceIsEmpty(t *testing.T) {
+	t.Fail()
+}
+
+func TestNamespaceCopy(t *testing.T) {
+	t.Fail()
+}
+
+func TestNamespaceJoinNamespace(t *testing.T) {
+	t.Fail()
+}
+
+func TestNamespaceJoinTable(t *testing.T) {
+	t.Fail()
+}
+
+func TestNamespaceGetTable(t *testing.T) {
+	t.Fail()
+}
+
+func TestNamespaceEquals(t *testing.T) {
+	t.Fail()
+}
+
+func TestEmptyTable(t *testing.T) {
+	t.Fail()
+}
+
+func TestRowMakeTable(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableForeachrow(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableCopy(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableAllRows(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableJoinTable(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableJoinRow(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableGetRow(t *testing.T) {
+	t.Fail()
+}
+
+func TestTableEquals(t *testing.T) {
+	t.Fail()
+}
+
+func TestEmptyRow(t *testing.T) {
+	t.Fail()
+}
+
+func TestEntryMakeRow(t *testing.T) {
+	t.Fail()
+}
+
+func TestRowCopy(t *testing.T) {
+	t.Fail()
+}
+
+func TestRowJoinRow(t *testing.T) {
+	t.Fail()
+}
+
+func TestRowGetEntry(t *testing.T) {
+	t.Fail()
+}
+
+func TestRowJoinEntry(t *testing.T) {
+	t.Fail()
+}
+
+func TestRowEquals(t *testing.T) {
+	t.Fail()
+}
+
+func TestEmptyEntry(t *testing.T) {
+	t.Fail()
+}
+
+func TestMakeEntry(t *testing.T) {
+	t.Fail()
+}
+
+func TestEntryJoinEntry(t *testing.T) {
+	t.Fail()
+}
+
+func TestEntryEquals(t *testing.T) {
+	t.Fail()
+}
+
+func TestEntryGetValues(t *testing.T) {
+	t.Fail()
+}
