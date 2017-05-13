0dccb117c8363f6e8a7c7af55437c25ac0d3e2b3
John Morrice <john@functorama.com>
2017-05-13 13:40:44 +0100

    Fix defect in RemoteStoreIndex

--- remote_store.go
+++ remote_store.go
@@ -1,6 +1,9 @@
 package godless
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 //go:generate mockgen -destination mock/mock_remote_store.go -imports lib=github.com/johnny-morrice/godless -self_package lib github.com/johnny-morrice/godless RemoteStore
 
@@ -15,6 +18,47 @@ type RemoteStore interface {
 
 type RemoteStoreAddress interface {
 	Path() string
+}
+
+type byPath []RemoteStoreAddress
+
+func (addrs byPath) Len() int {
+	return len(addrs)
+}
+
+func (addrs byPath) Swap(i, j int) {
+	addrs[i], addrs[j] = addrs[j], addrs[i]
+}
+
+func (addrs byPath) Less(i, j int) bool {
+	return addrs[i].Path() < addrs[j].Path()
+}
+
+func normalStoreAddress(addrs []RemoteStoreAddress) []RemoteStoreAddress {
+	uniq := uniqStoreAddress(addrs)
+	sort.Sort(byPath(uniq))
+	return uniq
+}
+
+func uniqStoreAddress(addrs []RemoteStoreAddress) []RemoteStoreAddress {
+	dedupe := map[string]RemoteStoreAddress{}
+
+	for _, a := range addrs {
+		path := a.Path()
+		if _, present := dedupe[path]; !present {
+			dedupe[path] = a
+		}
+	}
+
+	uniq := make([]RemoteStoreAddress, len(dedupe))
+
+	i := 0
+	for _, a := range dedupe {
+		uniq[i] = a
+		i++
+	}
+
+	return uniq
 }
 
 type RemoteNamespaceRecord struct {
@@ -93,7 +137,8 @@ func (rni RemoteNamespaceIndex) JoinNamespace(addr RemoteStoreAddress, namespace
 
 func (rni RemoteNamespaceIndex) addTable(table TableName, addr RemoteStoreAddress) RemoteNamespaceIndex {
 	if addrs, ok := rni.Index[table]; ok {
-		rni.Index[table] = append(addrs, addr)
+		normal := normalStoreAddress(append(addrs, addr))
+		rni.Index[table] = normal
 	} else {
 		rni.Index[table] = []RemoteStoreAddress{addr}
 	}
