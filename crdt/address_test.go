87843d7c35d1edd655f0dbf202b6ddd3391c13b4
John Morrice <john@functorama.com>
2017-06-30 09:45:09 +0100

    Tests for Link

--- crdt/address_test.go
+++ crdt/address_test.go
@@ -2,16 +2,93 @@ package crdt
 
 import (
 	"testing"
+
+	"github.com/johnny-morrice/godless/crypto"
+	"github.com/johnny-morrice/godless/internal/testutil"
 )
 
 func TestSignedLink(t *testing.T) {
-	t.FailNow()
+	const keyCount = 5
+	const text = "hello"
+	keys := generateTestKeys(keyCount)
+	duplicateKeys := append(keys, keys...)
+	badKeys := append(keys, crypto.PrivateKey{})
+
+	signed, err := SignedLink(text, keys)
+
+	testutil.AssertNil(t, err)
+	testutil.AssertLenEquals(t, len(keys), signed.Signatures())
+	testutil.AssertEquals(t, "Unexpected text", signed.Path(), text)
+
+	signed, err = SignedLink(text, duplicateKeys)
+
+	testutil.AssertNil(t, err)
+	testutil.AssertLenEquals(t, len(keys), signed.Signatures())
+	testutil.AssertEquals(t, "Unexpected text", signed.Path(), text)
+
+	signed, err = SignedLink(text, badKeys)
+
+	testutil.AssertNonNil(t, err)
+	testutil.AssertLenEquals(t, 0, signed.Signatures())
+	testutil.Assert(t, "Expected empty Path for failed SignedLink", signed.Path() == "")
 }
 
 func TestLinkIsVerifiedBy(t *testing.T) {
-	t.FailNow()
+	const keyCount = 5
+	const text = "hello"
+	myPriv, myPub, err := crypto.GenerateKey()
+
+	if err != nil {
+		panic(err)
+	}
+
+	notMyKeys := generateTestKeys(keyCount)
+
+	point, err := SignedLink(text, []crypto.PrivateKey{myPriv})
+
+	if err != nil {
+		panic(err)
+	}
+
+	isVerified := point.IsVerifiedBy(myPub)
+
+	testutil.Assert(t, "Expected verification", isVerified)
+
+	for _, notMyPriv := range notMyKeys {
+		isVerified = point.IsVerifiedBy(notMyPriv.GetPublicKey())
+		testutil.Assert(t, "Unexpected verification", !isVerified)
+	}
 }
 
 func TestLinkIsVerifiedByAny(t *testing.T) {
-	t.FailNow()
+	const keyCount = 5
+	const text = "hello"
+	myPriv, myPub, err := crypto.GenerateKey()
+
+	if err != nil {
+		panic(err)
+	}
+
+	notMyKeys := generateTestKeys(keyCount)
+	notMyPubKeys := make([]crypto.PublicKey, len(notMyKeys))
+
+	for i, notMyKey := range notMyKeys {
+		notMyPubKeys[i] = notMyKey.GetPublicKey()
+	}
+
+	allPubKeys := append(notMyPubKeys, myPub)
+
+	point, err := SignedLink(text, []crypto.PrivateKey{myPriv})
+
+	if err != nil {
+		panic(err)
+	}
+
+	isVerified := point.IsVerifiedByAny(allPubKeys)
+
+	testutil.Assert(t, "Expected verification", isVerified)
+
+	isVerified = point.IsVerifiedByAny(notMyPubKeys)
+
+	testutil.Assert(t, "Unexpected verification", !isVerified)
 }
--- crdt/point_test.go
+++ crdt/point_test.go
@@ -17,19 +17,19 @@ func TestSignedPoint(t *testing.T) {
 	signed, err := SignedPoint(text, keys)
 
 	testutil.AssertNil(t, err)
-	testutil.AssertLenEquals(t, len(keys), signed.Signatures)
-	testutil.AssertEquals(t, "Unexpected text", signed.Text, text)
+	testutil.AssertLenEquals(t, len(keys), signed.Signatures())
+	testutil.AssertEquals(t, "Unexpected text", signed.Text(), text)
 
 	signed, err = SignedPoint(text, duplicateKeys)
 
 	testutil.AssertNil(t, err)
-	testutil.AssertLenEquals(t, len(keys), signed.Signatures)
-	testutil.AssertEquals(t, "Unexpected text", signed.Text, text)
+	testutil.AssertLenEquals(t, len(keys), signed.Signatures())
+	testutil.AssertEquals(t, "Unexpected text", signed.Text(), text)
 
 	signed, err = SignedPoint(text, badKeys)
 
 	testutil.AssertNonNil(t, err)
-	testutil.AssertLenEquals(t, 0, signed.Signatures)
+	testutil.AssertLenEquals(t, 0, signed.Signatures())
 	testutil.Assert(t, "Expected empty text for failed SignedPoint", signed.Text() == "")
 }
 
