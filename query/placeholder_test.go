45702e37b22c132e4edac3a06c29080db7f64c59
John Morrice <john@functorama.com>
2017-07-31 19:55:44 +0100

    Add test skeletons for placeholders

---
+++ query/placeholder_test.go
@@ -0,0 +1,53 @@
+package query
+
+import (
+	"testing"
+)
+
+func TestNamePlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestJoinRowKeyPlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestJoinValuePlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestJoinKeyPlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestLimitPlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestPredicatePlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestPredicateRowKeyPlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestPredicateKeyPlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestPredicateLiteralPlaceholder(t *testing.T) {
+	t.FailNow()
+}
+
+func TestPlaceholderNumberMatch(t *testing.T) {
+	t.FailNow()
+}
+
+func TestPlaceholderTypeMatch(t *testing.T) {
+	t.FailNow()
+}
+
+func TestLongTextPlaceholders(t *testing.T) {
+	t.FailNow()
+}
