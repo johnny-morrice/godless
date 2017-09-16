a715aa9a7104c2ba97c0fa24ae00a4b9cff29b2b
John Morrice <john@functorama.com>
2017-09-16 14:24:10 +0100

    Implement profiling webservice

---
+++ api/profiler.go
@@ -0,0 +1,69 @@
+package api
+
+import (
+	"sync"
+)
+
+type Profiler interface {
+	NewTimer(name string) ProfileTimer
+}
+
+type ProfileTimer interface {
+	Start()
+	Stop()
+}
+
+type TimerList struct {
+	sync.Mutex
+	timers []ProfileTimer
+}
+
+func (list *TimerList) StopAllTimers() {
+	list.Lock()
+	defer list.Unlock()
+
+	for _, timer := range list.timers {
+		if timer != nil {
+			timer.Stop()
+		}
+	}
+}
+
+func (list *TimerList) StartTimer(timer ProfileTimer) {
+	list.Lock()
+	defer list.Unlock()
+
+	written := list.writeOverNil(timer)
+	if !written {
+		list.timers = append(list.timers, timer)
+	}
+
+	timer.Start()
+}
+
+func (list *TimerList) StopTimer(timer ProfileTimer) {
+	list.Lock()
+	defer list.Unlock()
+
+	timer.Stop()
+	list.writeNil(timer)
+}
+
+func (list *TimerList) writeNil(timer ProfileTimer) {
+	for i, other := range list.timers {
+		if timer == other {
+			list.timers[i] = nil
+		}
+	}
+}
+
+func (list *TimerList) writeOverNil(timer ProfileTimer) bool {
+	for i, other := range list.timers {
+		if other == nil {
+			list.timers[i] = timer
+			return true
+		}
+	}
+
+	return false
+}
--- http/profile_web_service.go
+++ http/profile_web_service.go
@@ -1,1 +1,61 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/johnny-morrice/godless/api"
+)
+
+type ProfilingWebService struct {
+	ProfilingWebServiceOptions
+	wsHandler http.Handler
+
+	timerList *api.TimerList
+}
+
+type ProfilingWebServiceOptions struct {
+	WS   api.WebService
+	Prof api.Profiler
+}
+
+func MakeProfilingWebService(options ProfilingWebServiceOptions) api.WebService {
+	if options.WS == nil {
+		options.WS = MakeWebService(api.WebServiceOptions{})
+	}
+
+	if options.Prof == nil {
+		panic("options.Prof was nil")
+	}
+
+	return &ProfilingWebService{
+		ProfilingWebServiceOptions: options,
+		wsHandler:                  options.WS.GetApiRequestHandler(),
+		timerList:                  &api.TimerList{},
+	}
+}
+
+func (service *ProfilingWebService) SetOptions(options api.WebServiceOptions) {
+	service.WS.SetOptions(options)
+}
+
+func (service *ProfilingWebService) GetApiRequestHandler() http.Handler {
+	return http.HandlerFunc(service.handleApiRequest)
+}
+
+func (service *ProfilingWebService) handleApiRequest(rw http.ResponseWriter, req *http.Request) {
+	const profileName = __PROFILE_WEB_SERVICE_NAME + ".handleApiRequest"
+
+	timer := service.Prof.NewTimer(profileName)
+
+	service.timerList.StartTimer(timer)
+	defer service.timerList.StopTimer(timer)
+
+	service.wsHandler.ServeHTTP(rw, req)
+}
+
+func (service *ProfilingWebService) Close() {
+	defer service.timerList.StopAllTimers()
+	service.WS.Close()
+}
+
+const __PROFILE_WEB_SERVICE_NAME = "ProfilingWebService"
